006d5db7cfbaf98d0f3cae6b9fccf5188d6cff44
Valentyna <valentina.mogort@gmail.com>
2018-11-09 09:43:32 +0000

    create carousel for web

--- js/index.js
+++ js/index.js
@@ -1,31 +1,26 @@
-(function () {
+(function() {
 
-document.querySelector('.form button[type=submit]').addEventListener('click',submitForm);
-  function submitForm (e){
-   e.preventDefault();
-    const xhr = new XMLHttpRequest();
-     xhr.onreadystatechange = function() {
-         if (xhr.readyState === 4 && xhr.status ===200 ){
-             document.querySelector('.message-box').innerHTML = ' Hi! Nice to meet you! <br>We will contact you soon!';
-         }else if (xhr.readyState === 4 && xhr.status !== 200){
-             document.querySelector('.message-box').innerHTML += 'error!';
-         }
-     }
-     xhr.open('POST', 'login.php', true);
-         const form = document.querySelector('.form');
-         const data = new FormData(form);
-         xhr.send(data);
-  }
- $(document).ready(function() {
-	$(".fancybox-thumb").fancybox({
-		prevEffect	: 'none',
-		nextEffect	: 'none',
-		helpers	: {
-			thumbs	: {
-				width	: 50,
-				height	: 50
-			}
-		}
-	});
-});
-})();
+    document.querySelector('.form button[type=submit]').addEventListener('click', submitForm);
+
+    function submitForm(e) {
+        e.preventDefault();
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                document.querySelector('.message-box').innerHTML = ' Hi! Nice to meet you! <br>We will contact you soon!';
+            } else if (xhr.readyState === 4 && xhr.status !== 200) {
+                document.querySelector('.message-box').innerHTML += 'error!';
+            }
+        }
+        xhr.open('POST', 'login.php', true);
+        const form = document.querySelector('.form');
+        const data = new FormData(form);
+        xhr.send(data);
+    }
+    $('.main-carousel').flickity({
+
+        cellAlign: 'center',
+        contain: true
+    });
+}
+)();
