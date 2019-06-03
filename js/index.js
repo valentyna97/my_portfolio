4d984bafebd59d55bc92508a822b561f3df33245
valentyna97 <44020908+valentyna97@users.noreply.github.com>
2019-06-03 17:07:14 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,18 +1,8 @@
-// (function() {
-//     $('.main-carousel').flickity({
-//
-//         cellAlign: 'center',
-//         contain: true
-//     });
-// }
-// )();
-$(function() {
-    // Owl Carousel
-    var owl = $(".owl-carousel");
-    owl.owlCarousel({
-        items: 1,
-        margin: 10,
-        loop: true,
-        nav: true
+(function() {
+    $('.main-carousel').flickity({
+
+        cellAlign: 'center',
+        contain: true
     });
-});
+}
+)();
